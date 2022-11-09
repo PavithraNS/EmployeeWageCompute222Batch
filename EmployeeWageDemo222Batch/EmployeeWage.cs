12d93f8393a6683f575f0a5c8486a9cbb1b212f7
PavithraNS <nspavi1991@gmail.com>
2022-11-09 09:06:27 +0530

    Adding Welcome message into master Branch

---
+++ EmployeeWageDemo222Batch/EmployeeWage.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageDemo222Batch
+{
+    class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation");
+            Console.ReadLine();
+        }
+    }
+}
