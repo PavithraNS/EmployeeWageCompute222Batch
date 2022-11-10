74373e0e4e2162a9feb80dfe4e2f7bc5a3edb64f
PavithraNS <nspavi1991@gmail.com>
2022-11-10 08:44:39 +0530

    Adding UC4 code

--- EmployeeWageDemo222Batch/EmployeeWage.cs
+++ EmployeeWageDemo222Batch/EmployeeWage.cs
@@ -15,20 +15,26 @@ namespace EmployeeWageDemo222Batch
             Console.WriteLine("Welcome to EmployeeWage Computation");
             //UC1 
             const int EMP_RATE_PER_HR = 20;
-            int employeePresent = 1;
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
             int empWage = 0;
             int empHrs = 0;
 
             Random r = new Random();
-            int employeeInput = r.Next(0, 2);
-            if (employeePresent == employeeInput)
+            int employeeInput = r.Next(0, 3);
+            switch(employeeInput)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
+                case FULL_TIME:
+                    Console.WriteLine("FullTime Employee is Present");
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("PartTime Employee is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
             //UC2
             empWage = EMP_RATE_PER_HR * empHrs;
